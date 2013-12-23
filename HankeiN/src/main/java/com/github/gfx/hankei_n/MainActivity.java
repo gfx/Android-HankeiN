88d6034ce0cf8bf4b37e2972bf875c4c495cf4ba
Fuji, Goro (gfx) <gfuji@cpan.org>
2013-12-24 00:52:36 +0900

    impl

--- HankeiN/src/main/java/com/github/gfx/hankei_n/MainActivity.java
+++ HankeiN/src/main/java/com/github/gfx/hankei_n/MainActivity.java
@@ -1,28 +1,79 @@
 package com.github.gfx.hankei_n;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements GoogleMap.OnMyLocationChangeListener, GoogleMap.OnMapLongClickListener {
     private static final String projectUrl = "http://github.com/gfx/Android-HankeiN";
+
+    static final float MAP_ZOOM = 14f;
+
+    static final float DEFAULT_RADIUS = 1.5f;
+
+    private GoogleMap map;
+    private Prefs prefs;
+
+    private boolean myLocationInitialized = false;
+
+    private Marker mapMarker;
+    private Circle mapCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        prefs = new Prefs(this);
+
         final MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        final GoogleMap googleMap = mapFragment.getMap();
-        googleMap.setMyLocationEnabled(true);
+        map = mapFragment.getMap();
+        map.setMyLocationEnabled(true);
+
+        map.setOnMyLocationChangeListener(this);
+        map.setOnMapLongClickListener(this);
+
+        setTitle(getRadius());
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final float prevLatitude = prefs.get("prevLatitude", 0.0f);
+        final float prevLongitude = prefs.get("prevLongitude", 0.0f);
+
+        if (prevLatitude != 0.0f || prevLongitude != 0.0) {
+            setMyLocation(prevLatitude, prevLongitude, false);
+        }
+
+        final float pointedLatitude = prefs.get("pointedLatitude", 0.0f);
+        final float pointedLongitude = prefs.get("pointedLongitude", 0.0f);
+
+        if (pointedLatitude != 0.0f || pointedLongitude != 0.0) {
+            setCircle(new LatLng(pointedLatitude, pointedLongitude));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,6 +94,27 @@ public class MainActivity extends Activity {
     }
 
     private boolean openSettingView() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        dialog.setTitle("半径の設定 (km)");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        input.setText(String.valueOf(getRadius()));
+        dialog.setView(input);
+
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                try {
+                    float value = Float.parseFloat(input.getText().toString());
+                    setRadius(value);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "エラー: 数値を入力してください", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        dialog.show();
         return true;
     }
 
@@ -52,5 +124,77 @@ public class MainActivity extends Activity {
         overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
 
         return true;
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Toast.makeText(this, "Point!", Toast.LENGTH_SHORT).show();
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(100);
+
+        prefs.put("pointedLatitude", (float) latLng.latitude);
+        prefs.put("pointedLongitude", (float) latLng.longitude);
+
+        setCircle(latLng);
+    }
+
+    private void setCircle(LatLng latLng) {
+        final MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Here"); // TODO: set address name
+        if (mapMarker != null) {
+            mapMarker.remove();
+        }
+        mapMarker = map.addMarker(markerOptions);
+
+        final CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(latLng);
+        circleOptions.radius(getRadius() * 1000);
+        circleOptions.strokeWidth(1);
+        circleOptions.strokeColor(0x990099ee);
+        circleOptions.fillColor(0x110099ee);
+        circleOptions.strokeWidth(3);
+        if (mapCircle != null) {
+            mapCircle.remove();
+        }
+        mapCircle = map.addCircle(circleOptions);
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+        prefs.put("prevLatitude", (float) location.getLatitude());
+        prefs.put("prevLongitude", (float) location.getLongitude());
+
+        if (myLocationInitialized) return;
+        myLocationInitialized = true;
+
+        setMyLocation(location.getLatitude(), location.getLongitude(), true);
+    }
+
+    private void setMyLocation(double lat, double lng, boolean animation) {
+        final LatLng latlng = new LatLng(lat, lng);
+        final CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, MAP_ZOOM);
+
+        if (animation) {
+            map.animateCamera(update);
+        } else {
+            map.moveCamera(update);
+        }
+    }
+
+    private void setTitle(float radius) {
+        setTitle(getResources().getString(R.string.app_title_template, radius));
+    }
+
+    private float getRadius() {
+        return prefs.get("radius", DEFAULT_RADIUS);
+    }
+
+    private void setRadius(float radius) {
+        setTitle(radius);
+        prefs.put("radius", radius);
+
+        final LatLng latLng = new LatLng(prefs.get("pointedLatitude", 0f), prefs.get("pointedLongitude", 0f));
+        setCircle(latLng);
     }
 }
---
+++ HankeiN/src/main/java/com/github/gfx/hankei_n/Prefs.java
@@ -0,0 +1,52 @@
+package com.github.gfx.hankei_n;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Prefs {
+    final private SharedPreferences sharedPrefs;
+
+    public Prefs(Context context) {
+        sharedPrefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+    }
+
+    public void put(String key, boolean value) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    public boolean get(String key, boolean defValue) {
+        return sharedPrefs.getBoolean(key, defValue);
+    }
+
+    public void put(String key, long value) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putLong(key, value);
+        editor.commit();
+    }
+
+    public long get(String key, long defValue) {
+        return sharedPrefs.getLong(key, defValue);
+    }
+
+    public void put(String key, float value) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putFloat(key, value);
+        editor.commit();
+    }
+
+    public float get(String key, float defValue) {
+        return sharedPrefs.getFloat(key, defValue);
+    }
+
+    public void put(String key, String value) {
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public String get(String key, String defValue) {
+        return sharedPrefs.getString(key, defValue);
+    }
+}
