4d53e2ef67ee59ec45676ac0859fa98e47389244
FUJI Goro (gfx) <gfuji@cpan.org>
2015-05-25 23:19:27 +0900

    rename things

--- HankeiN/src/main/java/com/github/gfx/hankei_n/activity/MainActivity.java
+++ HankeiN/src/main/java/com/github/gfx/hankei_n/activity/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawer);
+        setContentView(R.layout.activity_main);
 
         ButterKnife.inject(this);
 
